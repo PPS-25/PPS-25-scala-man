d9bfad5faae10e14ad6c7b53177c3fcac48d7e2c
Matilde D'Antino <matildedantino@gmail.com>
2026-08-09 20:17:28 +0200

    feat(model): configure how long each bonus effect lasts

---
+++ src/main/scala/it/unibo/pps/scalaman/model/BonusDuration.scala
@@ -0,0 +1,18 @@
+package it.unibo.pps.scalaman.model
+
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+
+/** How long the effect granted by a bonus lasts. */
+trait BonusDuration:
+  /** How long an effect lasts from the moment it is granted. */
+  def of(effect: BonusEffect): FiniteDuration
+
+object BonusDuration:
+  private val SlowDownLasts: FiniteDuration = 5.seconds
+  private val InvulnerabilityLasts: FiniteDuration = 8.seconds
+
+  /** The durations the game is played with. */
+  given standardDurations: BonusDuration with
+    def of(effect: BonusEffect): FiniteDuration = effect match
+      case BonusEffect.SlowDown        => SlowDownLasts
+      case BonusEffect.Invulnerability => InvulnerabilityLasts
