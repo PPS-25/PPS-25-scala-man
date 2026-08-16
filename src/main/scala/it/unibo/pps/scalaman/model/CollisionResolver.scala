fcaea5931959b7d11260b7564b44474735bfe55b
GaiaMazzoni <gaia.mazzoni2@studio.unibo.it>
2026-08-16 19:19:08 +0200

    refactor: change collision resolver to fit the new organization of collision handling.

--- src/main/scala/it/unibo/pps/scalaman/model/CollisionResolver.scala
+++ src/main/scala/it/unibo/pps/scalaman/model/CollisionResolver.scala
@@ -5,25 +5,9 @@ import it.unibo.pps.scalaman.model.map.ValidatedMap
 
 object CollisionResolver:
 
-  /** Resolves one collision by delegating to its corresponding game rule.
-    * @param entity
-    *   the collision was checked for.
-    * @param collision
-    *   the type of collision to resolve.
-    * @param map
-    *   the validated map.
-    * @return
-    *   the entity after the effect has been applied.
+  /** Teleports the player from one end of a teleport to the other.
     */
-  def resolve(entity: MovingEntity, collision: Collision, map: ValidatedMap): MovingEntity =
-    collision match
-      case Teleport(c)           => resolveTeleportCollision(entity, c, map)
-      case Collision.Wall        => entity
-      case Collision.Collectible => entity
-      case Collision.Enemy       => entity
-      case Collision.Bonus(_)    => entity
-
-  private def resolveTeleportCollision(
+  def teleported(
       entity: MovingEntity,
       code: Int,
       map: ValidatedMap
